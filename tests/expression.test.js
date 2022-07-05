67dc9e57914bbfc0a5b3ffeb2776c3c6571412ae
helloitsian <15301476+helloitsian@users.noreply.github.com>
2022-07-04 21:22:07 -0500

    simplified code

--- tests/expression.test.js
+++ tests/expression.test.js
@@ -1,5 +1,7 @@
 const ExpressionTest = () => {
   const message = "hello";
+  const func = (msg) => msg + " world";
+  const condition = true;
   return (
     <div>
       <div>
@@ -10,6 +12,15 @@ const ExpressionTest = () => {
       </div>
       <div>
         {`${message + "!"}`}
+      </div>
+      <div>
+        {"another message"}
+      </div>
+      <div>
+        {func(message)}
+      </div>
+      <div>
+        {condition ? "true" : "false"}
       </div>
     </div>
   );
